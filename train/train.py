d9bb6e4743d75e48f3be3475acd3c1aad5c8bede
seblful <alesha.chimba@gmail.com>
2023-10-28 00:40:31 +0300

    [EDIT] minor changes

--- train/train.py
+++ train/train.py
@@ -10,16 +10,16 @@ SAVE_MODEL_PATH = os.path.join(HOME, 'runs')
 PRETRAINED_MODEL_NAME = "stabilityai/stable-diffusion-2-1"
 
 WHAT_TO_TEACH = "style"
-PLACEHOLDER_TOKEN = "custom-art"
+PLACEHOLDER_TOKEN = "mvQypyI9Sqnnwve1FH"
 INITIALIZER_TOKEN = "art"
 
 # Setting up all training args
 HYPERPARAMETERS = {
     "learning_rate": 5e-04,
     "scale_lr": True,
-    "max_train_steps": 2000,
-    "save_steps": 250,
-    "train_batch_size": 4,
+    "max_train_steps": 100,
+    "save_steps": 10,
+    "train_batch_size": 3,
     "gradient_accumulation_steps": 1,
     "gradient_checkpointing": True,
     "mixed_precision": "fp16",
@@ -30,7 +30,7 @@ HYPERPARAMETERS = {
 def main():
     train_dataset = TextualInversionDataset(
         data_root=IMAGES_PATH,
-        size=vae.sample_size,
+        size=768,  # vae.sample_size,
         pretrained_model_name=PRETRAINED_MODEL_NAME,
         learnable_property=WHAT_TO_TEACH,
         placeholder_token=PLACEHOLDER_TOKEN,
@@ -46,6 +46,8 @@ def main():
                       placeholder_token=PLACEHOLDER_TOKEN,
                       initializer_token=INITIALIZER_TOKEN,
                       pretrained_model_name=PRETRAINED_MODEL_NAME)
+
+    trainer.accelerate_train()
 
 
 if __name__ == "__main__":
