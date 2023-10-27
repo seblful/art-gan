4c15879a00b2b7039d7143c03e7bae00401b2f69
seblful <alesha.chimba@gmail.com>
2023-10-28 00:26:39 +0300

    [ADD] create gradio app

--- process/app.py
+++ process/app.py
@@ -1,54 +1,54 @@
+from generator import ImagesGenerator
+
 import os
-from PIL import Image
-import torch
-from diffusers import DPMSolverMultistepScheduler, StableDiffusionPipeline
+import numpy as np
+import gradio as gr
 
 HOME = os.getcwd()
-OUTPUT_DIR = os.path.join(HOME, 'pred-images')
-MODEL_NAME = "a.bin"
-PRETRAINED_MODEL_PATH = os.path.join(HOME, 'models', MODEL_NAME)
-SCHEDULER_MODEL_PATH = os.path.join(HOME, 'models')
+PRETRAINED_MODEL_PATH = os.path.join(HOME, 'models')
 
 
-def image_grid(imgs, rows, cols):
-    assert len(imgs) == rows*cols
-
-    w, h = imgs[0].size
-    grid = Image.new('RGB', size=(cols*w, rows*h))
-    grid_w, grid_h = grid.size
-
-    for i, img in enumerate(imgs):
-        grid.paste(img, box=(i % cols*w, i//cols*h))
-    return grid
+prompt = "people in russia in the bright square 8k hiqh quality in the style <mvQypyI9Sqnnwve1FH>"
 
 
-# Set up the pipeline
-pipe = StableDiffusionPipeline.from_pretrained(
-    PRETRAINED_MODEL_PATH,
-    scheduler=DPMSolverMultistepScheduler.from_pretrained(
-        PRETRAINED_MODEL_PATH, subfolder="scheduler"),
-    torch_dtype=torch.float16,
-).to("cuda")
+def generate_images(input_text):
+    # Generate four images using stable diffusion
+    images = []
+    for _ in range(4):
+        # Replace this code with your own stable diffusion implementation
+        # This is just a placeholder code that generates random noise
+        image = np.random.randint(0, 256, size=(256, 256, 3), dtype=np.uint8)
+        images.append(image)
+    return images
 
 
-def generate_image_grid(prompt,
-                        num_samples=2,
-                        num_rows=1):
+def gradio_app():
+    with gr.Blocks(title="Art Image Generator", theme='base') as combined_sd:
+        with gr.Row():
+            with gr.Column():
+                image_generation_header = gr.Markdown('''
+                # Stable Diffusion Image Generation
+                                                      
+                Enter a prompt for what you would like Stable Diffusion to generate and click the "Generate Image" button to watch the result.
+                ''')
 
-    all_images = []
-    for _ in range(num_rows):
-        images = pipe([prompt] * num_samples, num_inference_steps=30,
-                      guidance_scale=7.5).images
-        all_images.extend(images)
+                user_prompt = gr.Textbox(
+                    label="What would you like to see?", placeholder="Enter some text")
 
-    grid = image_grid(all_images, num_rows, num_samples)
+                generate_image_button = gr.Button("Generate Image")
 
-    return grid
+            with gr.Column():
+                output_gallery = gr.Gallery(label="Generated Images")
+
+        generate_image_button.click(fn=generate_images,
+                                    inputs=[user_prompt],
+                                    outputs=[output_gallery])
+
+    combined_sd.launch()
 
 
 def main():
-    prompt = "a painting in the style of <custom-art>"
-    grid = generate_image_grid(prompt)
+    gradio_app()
 
 
 if __name__ == "__main__":
